dc894e8e75b39eae259bcad4ed1b68bdfc31162f
frandi <dev@rumahfrandi.com>
2016-07-17 14:05:50 +0700

    Add unit test for GetClassRoom

---
+++ test/Kelas.App.Core.Test/ClassRoomServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using Kelas.App.Core.ClassRoom;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Kelas.App.Core.Test
+{
+    public class ClassRoomServiceTest
+    {
+        [Fact]
+        public async void GetClassRoom_SuccessTest()
+        {
+            var idTest = Guid.NewGuid();
+            var itemTest = new ClassRoom.ClassRoom() { Id = idTest };
+
+            Mock<IClassRoomRepository> mockRepo = new Mock<IClassRoomRepository>();
+            mockRepo.Setup(r => r.GetById(idTest)).Returns(Task.FromResult(itemTest));
+
+            var service = new ClassRoomService(mockRepo.Object);
+            var item = await service.GetClassRoom(idTest);
+
+            Assert.Equal(item.Id, idTest);
+        }
+
+        [Fact]
+        public async void GetClassRoom_FailedTest()
+        {
+            Mock<IClassRoomRepository> mockRepo = new Mock<IClassRoomRepository>();
+         
+            var service = new ClassRoomService(mockRepo.Object);
+            var exception = await Record.ExceptionAsync(() => service.GetClassRoom(Guid.Empty));
+
+            Assert.NotNull(exception);
+        }
+    }
+}
