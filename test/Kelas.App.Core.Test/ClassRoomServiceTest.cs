556fcf8bff17eadef20dc874e8a0627c003f90f3
frandi <dev@rumahfrandi.com>
2016-07-17 22:16:08 +0700

    Add more unit test for ClassRoomService

--- test/Kelas.App.Core.Test/ClassRoomServiceTest.cs
+++ test/Kelas.App.Core.Test/ClassRoomServiceTest.cs
@@ -1,6 +1,8 @@
 ﻿using Kelas.App.Core.ClassRoom;
 using Moq;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -32,6 +34,60 @@ namespace Kelas.App.Core.Test
             var exception = await Record.ExceptionAsync(() => service.GetClassRoom(Guid.Empty));
 
             Assert.NotNull(exception);
+        }
+
+        [Fact]
+        public async void GetClassRooms_All_SuccessTest()
+        {
+            var testItems = new List<ClassRoom.ClassRoom>()
+            {
+                new ClassRoom.ClassRoom() { Id = Guid.NewGuid() },
+                new ClassRoom.ClassRoom() { Id = Guid.NewGuid() }
+            };
+
+            Mock<IClassRoomRepository> mockRepo = new Mock<IClassRoomRepository>();
+            mockRepo.Setup(r => r.Get(null)).Returns(Task.FromResult(testItems.AsEnumerable()));
+
+            var service = new ClassRoomService(mockRepo.Object);
+            var items = await service.GetClassRooms();
+
+            Assert.Equal(2, items.Count());
+        }
+
+        [Fact]
+        public async void GetClassRooms_WithCriteria_SuccessTest()
+        {
+            var testId = Guid.NewGuid();
+            var testItems = new List<ClassRoom.ClassRoom>()
+            {
+                new ClassRoom.ClassRoom() { Id = testId, Name = "Class A" },
+            };
+
+            var criteria = new List<SearchCriteria>()
+            {
+                new SearchCriteria() { SearchField = "Name", SearchOperator = SearchOperator.Equals, SearchValue = "Class A" }
+            };
+
+            Mock<IClassRoomRepository> mockRepo = new Mock<IClassRoomRepository>();
+            mockRepo.Setup(r => r.Get(It.Is<List<SearchCriteria>>(c => c.Equals(criteria)))).Returns(Task.FromResult(testItems.AsEnumerable()));
+            
+            var service = new ClassRoomService(mockRepo.Object);
+            var items = await service.GetClassRooms(criteria);
+
+            Assert.Equal(1, items.Count());
+            Assert.Equal(testId, items.First().Id);
+        }
+
+        [Fact]
+        public async void GetClassRooms_EmptyResult_SuccessTest()
+        {
+            Mock<IClassRoomRepository> mockRepo = new Mock<IClassRoomRepository>();
+            mockRepo.Setup(r => r.Get(It.IsAny<List<SearchCriteria>>())).Returns(Task.FromResult((new List<ClassRoom.ClassRoom>()).AsEnumerable()));
+
+            var service = new ClassRoomService(mockRepo.Object);
+            var items = await service.GetClassRooms(new List<SearchCriteria>());
+
+            Assert.Empty(items);
         }
     }
 }
